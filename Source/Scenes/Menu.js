9fa53d4bd7951a0627a2af35a66d17f3a6b58350
Dennis Moiseyev <dmoiseye@ucsc.edu>
2020-04-08 15:47:24 -0700

    Scenes and assets implemented

--- Source/Scenes/Menu.js
+++ Source/Scenes/Menu.js
@@ -1,6 +1,15 @@
-class Menu extends Phaser.Scenes {
+class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
+    }
+
+    create(){
+        //this references the scene that we are making (menu)
+        //displays menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        //launches the next scene
+        this.scene.start("playScene");
     }
 
 }
